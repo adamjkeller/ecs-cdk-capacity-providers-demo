bf0122df6d4bad49cb83058ee16d6eed73a97377
EC2 Default User <ec2-user@ip-172-31-44-135.us-west-2.compute.internal>
2021-05-27 17:43:45 +0000

    Done

--- python_app.py
+++ python_app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return f"OS Architecture: {os.uname().machine}"
+    return f'{{ "OS_Architecture": "{os.uname().machine}" }}'
 
 if __name__ == '__main__':
     app().run(host='0.0.0.0')
